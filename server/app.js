b93e2cb27b941839ccb528b8358447b99a6a6ad8
kaitlinmuth <kaitlin.muth@gmail.com>
2015-05-07 14:55:27 -0500

    Server functions

--- server/app.js
+++ server/app.js
@@ -1,4 +1,21 @@
 /**
  * Created by kaitlinmuth on 5/7/15.
  */
-console.log("Server started");
+var express = require('express');
+var app = express();
+
+var path = require('path');
+var jquery = require('jquery');
+// var bootstrap = require('bootstrap');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/*', function (request, response){
+   var file = request.params[0] || '/views/index.html';
+
+    response.sendFile(path.join(__dirname, './public/', file));
+});
+
+app.listen(app.get('port'), function(){
+    console.log("Listening on port: ",app.get('port'));
+});
