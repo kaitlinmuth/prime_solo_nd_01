a408fb35210c0b3de608332d55412aac9f5a810e
kaitlinmuth <kaitlin.muth@gmail.com>
2015-05-10 21:35:29 -0500

    School info is now stored in JSON object and selected/drawn like experience info

--- client/app.js
+++ client/app.js
@@ -2,22 +2,26 @@
  * Created by kaitlinmuth on 5/7/15.
  */
 var experienceHTML;
-var experienceData;
+var educationHTML
+var resumeData;
 
 function drawIcons (){
-    console.log("drawing icons!");
     $.get('/assets/data/data.json', function(data){
         for (var i=0; i<data.experience.length; i++){
             if (i%2 == 0) {$(".iconPicker").append("<div class='row'></div>");}
             $(".iconPicker").last().append("<div class='jobIcon col-md-6'><img src='/images/"+data.experience[i].image+"'</div>");
             $(".iconPicker").children('.jobIcon').last().data("index", [i]);
         }
+        for (var j=0; j<data.education.length; j++){
+            if (j%2 == 0) {$(".schoolIconPicker").append("<div class='row'></div>");}
+            $(".schoolIconPicker").last().append("<div class='schoolIcon col-md-6'><img src='/images/"+data.education[j].image+"'</div>");
+            $(".schoolIconPicker").children('.schoolIcon').last().data("index", [j]);
+        }
     });
 
 }
 
 function writeJobData(data){
-    console.log("writing job data");
     $(".jobShowcase").hide();
     $(".jobShowcase").html(experienceHTML);
     $(".job-icon").attr("src", "/images/"+data.image);
@@ -27,48 +31,38 @@ function writeJobData(data){
     $(".jobShowcase").slideDown();
 }
 
+function writeSchoolData(data){
+    $(".schoolShowcase").hide();
+    $(".schoolShowcase").html(educationHTML);
+    $(".school-icon").attr("src", "/images/"+data.image);
+    $(".school-name").text(data.name);
+    $(".school-degree-dates").text(data.degree+", "+data.date);
+    if (data.honors) {$(".school-honors").text(data.honors);}
+    if (data.description) {$(".school-description").html(data.description);}
+    $(".schoolShowcase").slideDown();
+}
+
 $(document).ready(function(){
     $.get('/views/template.html', function(data){
         experienceHTML = data;
         });
-    $.get('/assets/data/data.json', function(data) {
-        experienceData = data;
-        writeJobData(experienceData.experience[0]);
+    $.get('/views/template2.html', function(data){
+        educationHTML = data;
     });
-
-    drawIcons();
+    $.get('/assets/data/data.json', function(data) {
+        resumeData = data;
+        writeJobData(resumeData.experience[0]);
+        writeSchoolData(resumeData.education[0]);
+        drawIcons();
+    });
 
     $(".iconPicker").on('click', '.jobIcon', function(){
         var index = $(this).data("index");
-        writeJobData(experienceData.experience[index]);
+        writeJobData(resumeData.experience[index]);
     });
 
-});
-
-/* console.log(cityData, cityHtml);
-
-    $.get('data.json', function(data){
-        console.log("second request");
-        if (cityData === null) {
-            cityData = data;
-            for (i=0; i<cityData.locations.length; i++) {
-                $("#more-stuff").append(cityHtml);
-                $("#more-stuff").children().last().prepend("<img src='"+cityData.locations[i].image +"'>");
-                $("#more-stuff").find(".location-name").last().append("<strong>Name: </strong>" + cityData.locations[i].name);
-                $("#more-stuff").find(".location-population").last().append("<strong>Population: </strong>" + cityData.locations[i].population);
-                $("#more-stuff").find(".location-area").last().append("<strong>Area: </strong>" + cityData.locations[i].area);
-                $("#more-stuff").find(".location-monsters").last().append("<strong>Monsters: </strong>" + cityData.locations[i].monsters);
-            }
-            console.log("data is", cityData);
-        } else {
-            console.log("Data is already set");
-        }
+    $(".schoolIconPicker").on('click', ".schoolIcon", function(){
+        var index = $(this).data("index");
+        writeSchoolData(resumeData.education[index]);
     });
 });
-
-$("#more-stuff").on('click', '.btn', function(){
-    $(this).parent().parent().fadeOut("slow", function(){
-        $(this).remove();
-    });
-});
-    */
